928bea655ec1c982f6d4d18bb944205b3a7352b7
Daniel Huebner <daniel_huebner@ymail.com>
2025-04-18 18:44:47 +0200

    added a log analytics workspace, as this resource is going to be used as a observability

---
+++ env/dev.tfvars
@@ -0,0 +1,1 @@
+instituion_name = "development"
--- main.tf
+++ main.tf
@@ -1,29 +1,11 @@
-resource "azurerm_resource_group" "az_rg_01" {
+resource "azurerm_resource_group" "main" {
   name     = "rg-${var.application_name}-${var.instituion_name}"
   location = var.primary_region
 }
-
-resource "random_string" "storageaccount_suffix" {
-  length  = 10
-  upper   = false
-  special = false
-}
-
-resource "azurerm_storage_account" "az_sa_01" {
-  name                     = "st-${random_string.storageaccount_suffix.result}"
-  resource_group_name      = azurerm_resource_group.az_rg_01.name
-  location                 = azurerm_resource_group.az_rg_01.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-
-resource "azurerm_storage_container" "blob_container_01" {
-  name                  = "important"
-  storage_account_id    = azurerm_storage_account.az_sa_01.id
-  container_access_type = "private"
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${var.application_name}-${var.instituion_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
