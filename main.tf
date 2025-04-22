1a3ac35fbe05141f0b7552c462acbc60cacdb58e
Daniel Huebner <daniel_huebner@ymail.com>
2025-04-22 16:42:14 +0200

    added a role assignment for myself

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${var.application_name}-${var.instituion_name}"
   location = var.primary_region
 }
-
+data "azurerm_client_config" "current" {}
 
 resource "random_string" "keyvault_stuffix" {
   length  = 6
@@ -14,6 +14,12 @@ resource "azurerm_key_vault" "main" {
   name                = "kv-${var.application_name}-${var.instituion_name}-${random_string.keyvault_stuffix.result}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  tenant_id           = "f761b35c-e362-4a79-95b0-70328476e5cf"
+  tenant_id           = data.azure_client_config.current.tenant.id
   sku_name            = "standard"
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
