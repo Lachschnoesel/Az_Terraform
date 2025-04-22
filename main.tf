6d43bc852c3c9cbe544622f9afafcfa8e8ec8852
Daniel Huebner <daniel_huebner@ymail.com>
2025-04-22 17:43:58 +0200

    added a variable for the subnet space and a cidr subnet function to automaticly get the ubnet spaces

--- main.tf
+++ main.tf
@@ -6,11 +6,38 @@ resource "azurerm_virtual_network" "main" {
   name                = "vnet-${var.application_name}-${var.instituion_name}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.44.0.0/22"]
+  address_space       = [var.vnet-adressspace]
+}
+
+locals {
+  first_addressspace  = cidrsubnet(var.vnet-adressspace, 2, 0)
+  second_addressspace = cidrsubnet(var.vnet-adressspace, 2, 1)
+  thrid_addressspace  = cidrsubnet(var.vnet-adressspace, 2, 0)
+  forth_addressspace  = cidrsubnet(var.vnet-adressspace, 2, 0)
+
 }
 resource "azurerm_subnet" "firstsubnet" {
   name                 = "snet-first"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.44.0.0/24"]
+  address_prefixes     = [local.first_addressspace]
 }
+resource "azurerm_subnet" "secoundsubnet" {
+  name                 = "snet-second"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.second_addressspace]
+}
+resource "azurerm_subnet" "thirdsubnet" {
+  name                 = "snet-third"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.thrid_addressspace]
+}
+resource "azurerm_subnet" "forthsubnet" {
+  name                 = "snet-forth"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.forth_addressspace] #each have 1/4 of the vnet, 251/255 addresses are aviable, 5 are preoccupied by azure
+}
+
