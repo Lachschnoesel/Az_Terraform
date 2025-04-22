f4996db1569be23f8b51451ffb239d9d4c7026ce
Daniel Huebner <daniel_huebner@ymail.com>
2025-04-22 18:19:25 +0200

    added a network interface card in the subnet, that was created in the previous branch, the public ip was added the nsc too

--- main.tf
+++ main.tf
@@ -9,3 +9,22 @@ resource "azurerm_public_ip" "vm1" {
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
 }
+data "azurerm_subnet" "first" {
+  name                 = "snet-first"
+  virtual_network_name = "vnet-network-dev"
+  resource_group_name  = "rg-network-dev"
+}
+
+
+resource "azurerm_network_interface" "vm1" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "public"
+    subnet_id                     = data.azurerm_subnet.first.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1.id
+  }
+}
