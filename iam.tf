2a94b7f09d41fb92ec8a10241c51758d11f3057c
Daniel Huebner <daniel_huebner@ymail.com>
2025-04-23 11:30:57 +0200

    assigned myself to the group to get the perms with the given group for the bastion login in to the vm

--- iam.tf
+++ iam.tf
@@ -1,7 +1,12 @@
-data "azure_client_config" "current" {}
+data "azuread_client_config" "current" {}
 
 resource "azuread_group" "remote_access" {
-  display_name     = "group${var.application_name}-${var.enviornment_name}"
+  display_name     = "group${var.application_name}-${var.instituion_name}"
   owners           = [data.azure_client_config.current.object_id]
   security_enabled = true
+}
+
+resource "azuread_group_member" "daniel_is_in_the_group" {
+  group_object_id  = azuread_group.remote_access_users.object_id
+  member_object_id = data.azuread_client_config.current.object_id
 }
